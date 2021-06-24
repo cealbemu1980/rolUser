65d95b6fd0e5f2dbeebb58cf14e509302e888d65
cealbemu7 <cebemu7@gmail.com>
2021-06-24 15:06:13 -0500

    commit inicial con eclipse.

---
+++ demoAdminRole/src/main/java/com/example/demoAdminRole/DemoAdminRoleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoAdminRole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAdminRoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAdminRoleApplication.class, args);
+	}
+
+}
