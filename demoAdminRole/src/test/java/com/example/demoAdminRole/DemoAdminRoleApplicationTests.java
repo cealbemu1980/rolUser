f4ea9a57a03fa18e961fecd826941da88c9e17d6
cealbemu7 <cebemu7@gmail.com>
2021-06-24 15:06:13 -0500

    commit inicial con eclipse. Close #1

---
+++ demoAdminRole/src/test/java/com/example/demoAdminRole/DemoAdminRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoAdminRole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAdminRoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
